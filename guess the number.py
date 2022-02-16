dd6a1b6d24a2efadca1e4085097a8c58ddf1fb7c
Koryun-Harutyuyan <koryun.harutyunyan.05@mail.ru>
2022-02-16 20:53:41 +0400

    last commit in new version

--- guess the number.py
+++ guess the number.py
@@ -3,7 +3,7 @@ a = (random.randint(1,10))
 wrongs = 0
 print("Hello what's your name?")
 name = input()
-print("Okay"+name+"I am guessing a number between 1 and 10.")
+print("Okay "+name+" I am guessing a number between 1 and 10.")
 def my_function():
     if answer < a:
         print("Your answer is low.")
